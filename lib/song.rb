e17dc8f189a06903728fde51031c0fddaef4e88d
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-11-15 14:33:49 -0700

    commit tree c52b9f2959633d5647bbf2f54468c6d9a30b17b6

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,56 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    return song
+  end
+
+  def self.find_by_name(name)
+    all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if Song.find_by_name(name) == nil
+      return Song.create_by_name(name)
+    end
+    return Song.find_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort {|song_1, song_2| song_1.name <=> song_2.name}
+  end
+
+  def self.new_from_filename(full_song)
+    regex = full_song.scan(/(^[a-z ]+?)[\s-]+([a-z ]+?)\./i)
+    song = self.new
+    song.name = regex[0][1]
+    song.artist_name = regex[0][0]
+    return song
+  end
+
+  def self.create_from_filename(full_song)
+    regex = full_song.scan(/(^[a-z ]+?)[\s-]+([a-z ]+?)\./i)
+    song = self.create
+    song.name = regex[0][1]
+    song.artist_name = regex[0][0]
+    return song
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 end
